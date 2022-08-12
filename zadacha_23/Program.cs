f941c3dc3c25e1896dc86295f601771c04c1ff9f
Marina <mvalpeters@gmail.com>
2022-08-12 21:09:21 +0300

    zadacha_13

---
+++ zadacha_23/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+double [] GetCubeNumbers (int N)
+{
+    double [] numbers = new double [N];
+    double k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        k = Convert.ToDouble(i) + 1;
+        numbers[i] = Math.Pow(k,3);
+    }
+       return numbers;
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+double [] numbers = GetCubeNumbers (number);
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine (numbers[i]);
+}
