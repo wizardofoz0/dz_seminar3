6d884406083da6b327a2429834594039a9f9da85
Marina <mvalpeters@gmail.com>
2022-08-15 20:35:18 +0300

    practice 4

---
+++ pr2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+4  -> 24
+5  -> 120*/
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+
+WriteLine(FunctionMultiply (Convert.ToInt32(ReadLine())));
+
+int FunctionMultiply(int n)
+{
+    int result = 1;
+
+for (int i=1; i<=n; i++)
+{
+result*=i;
+}
+
+    return result;
+}
